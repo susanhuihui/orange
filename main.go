1c7811dcf0471376216e43d82e46c11354ca3633
susanhuihui <496104308@qq.com>
2015-12-25 12:24:59 +0800

    up_orangejs
    uporangejs,main

--- main.go
+++ main.go
@@ -11,9 +11,11 @@ func init() {
 	orm.RegisterDriver("mysql", orm.DR_MySQL) //注册mysql Driver//注册数据库连接
 	//orm.RegisterDataBase("default", "mysql", "root:33c88fd2b0@tcp(127.0.0.1:3306)/onlineeducation") //本地数据库：数据库别名，driverName,对应链接字符串
 	//orm.RegisterDataBase("default", "mysql", "fankunedu:33C88FD2B0@tcp(101.200.75.221:3306)/onlineeducation") //本地测试：数据库别名，driverName,对应链接字符串
-	orm.RegisterDataBase("default", "mysql", "root:33C88FD2B0@tcp(127.0.0.1:3306)/onlineeducation") //发布后：数据库别名，driverName,对应链接字符串
+	//orm.RegisterDataBase("default", "mysql", "root:33C88FD2B0@tcp(127.0.0.1:3306)/onlineeducation") //发布后：数据库别名，driverName,对应链接字符串
 	//orm.RegisterDataBase("default", "mysql", "fankunedu:33C88FD2B0@tcp(101.200.75.221:3306)/onlineeducation?charset=utf8&loc="+url.QueryEscape("Asia/Shanghai"), 30)
-	//orm.RegisterDataBase("default", "mysql", "fankunedu:33C88FD2B0@tcp(101.200.75.221:3306)/onlineeducation?charset=utf8&loc=Local", 30)
+	orm.RegisterDataBase("default", "mysql", "fankunedu:33C88FD2B0@tcp(101.200.75.221:3306)/onlineeducation?charset=utf8&loc=Local", 30)
+	// 2015/12/15
+	// 李向哲修改连库字符串为本地时区解析
 
 }
 
